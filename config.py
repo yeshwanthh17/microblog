1874d5c86d0c31cdb50cadbd368c80abfe61986b
yeshwanthh17 <yaswanthr538@gmail.com>
2025-04-05 21:17:10 +0530

    test rebase

--- config.py
+++ config.py
@@ -6,3 +6,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
+
+#
+#cl#   'sqlite:///' + os.path.join(basedir, 'app.db')
+#
